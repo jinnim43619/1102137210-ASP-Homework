b0086cd688680e63dd5d6deaea87490ab1d22b26
jinnim43619 <jinnim43619@gmail.com>
2016-03-27 23:14:21 +0800

    first commit

---
+++ 1102137210/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1102137210.MvcApplication" Language="C#" %>
---
+++ 1102137210/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1102137210.Startup))]
+namespace _1102137210
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
